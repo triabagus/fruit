f1aab7f47f49b0af6f4500de7dcfe75e32104db3
triabagus <triatop9@gmail.com>
2023-09-12 19:47:02 +0700

    Change background scroll

--- assets/js/main.js
+++ assets/js/main.js
@@ -31,7 +31,14 @@ const linkAction = () => {
 navLink.forEach((n) => n.addEventListener("click", linkAction));
 
 /*=============== SHADOW HEADER ===============*/
-
+const shadowHeader = () => {
+  const header = document.getElementById("header");
+  // When the scroll is greater than 50 viewport height, add the scroll-header class
+  this.scrollY >= 50
+    ? header.classList.add("shadow-header")
+    : header.classList.remove("shadow-header");
+};
+window.addEventListener("scroll", shadowHeader);
 
 /*=============== SHOW SCROLL UP ===============*/ 
 
